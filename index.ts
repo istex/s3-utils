c79a54404457913e598f9c5a40025f42556d3ee0
ZackPedretti <zackpedretti52@gmail.com>
2026-04-08 16:43:29 +0200

    fix: export all method from index

--- index.ts
+++ index.ts
@@ -1,3 +1,2 @@
-import { getS3Client, putFileToS3, getFileFromS3 } from './src/S3Management'
+export { getS3Client, putFileToS3, getFileFromS3, getEnvConfig, getSHA1OfObject, getListObjectsFromS3, getHeadObjectFromS3, s3FileExists } from './src/S3Management'
 
-export { getS3Client, putFileToS3, getFileFromS3 }
