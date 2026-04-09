bf8f118ea60950b4ac6ceb1514892163e976ffd1
ZackPedretti <zackpedretti52@gmail.com>
2026-04-09 15:44:02 +0200

    chore: rename test file

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,7 +8,7 @@ import {
   getHeadObjectFromS3,
   getEnvConfig,
   getSHA1OfObject,
-} from "../src/index";
+} from "../src/index.js";
 import { S3Client, type _Object, type S3ClientConfig } from "@aws-sdk/client-s3";
 import { createS3Client } from "mock-aws-s3-v3";
 import fs from "fs";
